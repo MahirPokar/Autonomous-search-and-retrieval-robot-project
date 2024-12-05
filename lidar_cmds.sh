77d6003cba1f9ebb60f27cf390901329c2497c50
Euan Baldwin <116883900+EuanBaldwin@users.noreply.github.com>
2024-12-05 13:00:43 +0000

    Delete lidar_cmds.sh

--- lidar_cmds.sh
+++
@@ -1,7 +0,0 @@
-cd ~/ros2_ws
-colcon build #--symlink-install
-source ./install/setup.bash
-sudo chmod 777 /dev/ttyUSB0
-cd src/rplidar_ros/
-source scripts/create_udev_rules.sh
-ros2 launch rplidar_ros view_rplidar_a2m12_launch.py
