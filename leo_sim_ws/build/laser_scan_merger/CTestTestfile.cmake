752994ef79e20d1edf26c1d89f4e7ccf0db8bc15
Mahir Pokar <mahirpokar7745@gmail.com>
2025-04-12 07:46:40 -0400

    leo_sim

--- leo_sim_ws/build/laser_scan_merger/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/bestteamever/leo_sim_ws/src/scan_merger_2
-# Build directory: /home/bestteamever/leo_sim_ws/build/laser_scan_merger
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
--- leo_sim_ws/build/navigation_demos/prefix_override/sitecustomize.py
+++
@@ -1,4 +0,0 @@
-import sys
-if sys.prefix == '/usr':
-    sys.real_prefix = sys.prefix
-    sys.prefix = sys.exec_prefix = '/home/bestteamever/leo_sim_ws/install/navigation_demos'
